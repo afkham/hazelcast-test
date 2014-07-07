2c84051b079e330343cd2beeff0d0ca2b21c5a5d
Afkham Azeez <azeez@wso2.com>
2014-07-07 17:31:36 +0530

    Added Hz code

--- src/main/java/org/wso2/hazelcast/HzTest.java
+++ src/main/java/org/wso2/hazelcast/HzTest.java
@@ -18,11 +18,73 @@
  */
 package org.wso2.hazelcast;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.*;
+
 /**
  * TODO: class level comment
  */
 public class HzTest {
+
+    public static final String DOMAIN = "wso2.hztest.domain";
+    public static final String HZ_MAP = "wso2.hztest.map";
+
     public static void main(String[] args) {
+        Config config = new Config();
+        GroupConfig groupConfig = config.getGroupConfig();
+        groupConfig.setName(DOMAIN);
+
+        NetworkConfig nwConfig = config.getNetworkConfig();
+        nwConfig.setPublicAddress("127.0.0.1");
+//        nwConfig.setPort(4001);
+
+        HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance(config);
+        hzInstance.getCluster().addMembershipListener(new MembershipListener() {
+            @Override
+            public void memberAdded(MembershipEvent membershipEvent) {
+                Member member = membershipEvent.getMember();
+                System.out.println("Member joined [" + member.getUuid() + "]: " + member.getInetSocketAddress().toString());
+            }
+
+            @Override
+            public void memberRemoved(MembershipEvent membershipEvent) {
+                Member member = membershipEvent.getMember();
+                System.out.println("Member left [" + member.getUuid() + "]: " + member.getInetSocketAddress().toString());
+            }
+        });
+
+        final IMap<String, Long> map = hzInstance.getMap(HZ_MAP);
+
+        /*new Thread() {
+
+            String[] keys = {"a", "b", "c", "d", "e", "f"};
+            int keyIndex = 0;
+
+            @Override
+            public void run() {
+                while (true) {
+                    if (keyIndex >= keys.length) {
+                        keyIndex = 0;
+                    }
+                    long start = System.currentTimeMillis();
+                    System.out.println(keys[keyIndex] + "=" + map.get(keys[keyIndex]));
+                    System.out.println("Hz map get took " + (System.currentTimeMillis() - start) + "ms");
+
+                    start = System.currentTimeMillis();
+                    map.put(keys[keyIndex], System.currentTimeMillis());
+                    System.out.println("Hz map put took " + (System.currentTimeMillis() - start) + "ms");
+
+                    keyIndex++;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();*/
 
     }
 }
